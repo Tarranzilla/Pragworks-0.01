4bb7dd189e5952ae57585a6cf424f94c26ecca1b
JOAO TARRAN <joaotarran@gmail.com>
2023-02-13 16:14:12 -0300

    update

---
+++ dist/react_test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    const [count, setCount] = React.useState(0);
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+        </div>
+    );
+}
+
+ReactDOM.render(<App></App>, document.getElementById("section-001-id"));
